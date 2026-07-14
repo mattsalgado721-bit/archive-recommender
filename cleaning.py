433aadda4cb742790bbce41dae57204b959bb61e
mattsalgado721-bit <mattsalgado721@gmail.com>
2026-07-13 17:15:20 -0700

    Finished writing embed.py *Setup the CLIP model, chromadb client, and the specific collction the data will be sent to *Extracted the embeddings of the images, metadata(brand, title), and their unique id *Sent the embedding data of each image to chromadb, using their unique id as their identifier *Updated the cleaning script to remove uncessay columns before finishing the embedding extraction *Created the .env file in order to connect to the chromadb api

--- cleaning.py
+++ cleaning.py
@@ -3,15 +3,18 @@ import pandas as pd
 
 df = pd.read_json("/home/administrator/.cache/kagglehub/datasets/crawlfeeds/images-extracted-from-fashion-website/versions/1/farfetch_fashion_dataset_images_crawlfeeds.json")
 
-df["image_file"] = df["image_file"].str.split("|").str[0].str.strip()
 
-print(df["image_file"])
+df["image_file"] = df["image_file"].str.split("|").str[0].str.strip()
 
 base_path = "/home/administrator/.cache/kagglehub/datasets/crawlfeeds/images-extracted-from-fashion-website/versions/1/images"
 df["absolute_path"] = df["image_file"].apply(lambda x : os.path.join(base_path, x))
+
+essential_columns = ["absolute_path", "uniq_id", "brand", "title", "url"]
+df_clean = df[essential_columns]
+
 
 try:
     os.mkdir("data")
 except OSError as e:
     print("Error: ", e)
-df.to_csv("data/catalog.csv", index=False)
+df_clean.to_csv("data/catalog.csv", index=False)
--- embed.py
+++ embed.py
@@ -1,19 +1,89 @@
+import os
 import torch
-from PIL import Image
+import pandas as pd
+import chromadb
+from PIL import Image, UnidentifiedImageError
 from transformers import CLIPProcessor, CLIPModel
+from dotenv import load_dotenv
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+load_dotenv()
 
+def read_file(path):
+    df = pd.read_csv(path)
+    df = df.dropna(subset=["absolute_path"])
+    return df.to_dict("records")
 
-path = "/home/administrator/.cache/kagglehub/datasets/crawlfeeds/images-extracted-from-fashion-website/versions/1/images/images/10851775_0.jpg"
-image = Image.open(path)
+def initialize_models():
+    client = chromadb.CloudClient(
+    api_key=os.getenv("CHROMA_API_KEY"),
+    tenant=os.getenv("CHROMA_TENANT"),
+    database=os.getenv("CHROMA_DATABASE")
+    )
+    collection = client.get_or_create_collection(name="images_collection")
 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-inputs = processor(images=image, return_tensors="pt").to(device)
-with torch.no_grad():
-    image_features = model.get_image_features(**inputs)
+    return collection, device, model, processor
 
-pooler_output = image_features.pooler_output
-print(pooler_output.shape)
+def extract_embedding(image_path, device, model, processor):
+    try:
+        image = Image.open(image_path)
+    except FileNotFoundError:
+        print(f"Error: file not found at {image_path}, continuing...")
+        return None
+    except UnidentifiedImageError:
+        print(f"Error: image at {image_path} cannot be opened and identified, continuing...")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}, continuing...")
+        return None
+
+    inputs = processor(images=image, return_tensors="pt").to(device)
+    with torch.no_grad():
+        image_features = model.get_image_features(**inputs)
+
+    return image_features.pooler_output[0].cpu().tolist()
+
+def upload_chunk_to_chroma(collection, ids, embeddings, metadata):
+    collection.add(
+    ids=ids,
+    embeddings=embeddings,
+    metadatas=metadata,
+    )
+
+def run_extraction(catalog, collection, device, model, processor):
+    ids_list = []
+    embeddings_list = []
+    metadata_list = []
+
+    chunk_size = 250
+
+    for row in catalog:
+        path = row["absolute_path"]
+        image_embeddings = extract_embedding(path, device, model, processor)
+
+        if image_embeddings is None:
+            continue
+
+        embeddings_list.append(image_embeddings)
+        metadata_list.append({"brand" : row["brand"], "title": row["title"]})
+        ids_list.append(str(row["uniq_id"]))
+
+        if len(ids_list) >= chunk_size:
+            upload_chunk_to_chroma(collection, ids_list, embeddings_list, metadata_list)
+            ids_list, embeddings_list, metadata_list = [], [], []
+
+    if len(ids_list) > 0: 
+        upload_chunk_to_chroma(collection, ids_list, embeddings_list, metadata_list)
+            
+
+def main():
+    collection, device, model, processor = initialize_models()
+    catalog = read_file("data/catalog.csv")
+    run_extraction(catalog, collection, device, model, processor)
+
+if __name__ == "__main__":
+    main()
+
